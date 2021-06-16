89124f449864bd8402bdbd922e7cfa8566ca9e75
sylvan008 <sylvan.climb@gmail.com>
2021-06-16 14:45:29 +0300

    module for full-size images is connected and added as a parameter to viewImagesThumbnails

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {createStubPublishedPhoto} from './data.js';
 import {viewImagesThumbnails} from './view-images-thumbnails.js';
+import {viewFullImage} from './view-full-image.js';
 
 const userPhotos = createStubPublishedPhoto(25, 3);
-viewImagesThumbnails(userPhotos);
+viewImagesThumbnails(userPhotos, viewFullImage);
--- js/view-images-thumbnails.js
+++ js/view-images-thumbnails.js
@@ -1,3 +1,5 @@
+import {noop} from './utils/utile.js';
+
 function createPictureThumbnail(template, {url, comments, likes}) {
   const pictureElement = template.querySelector('.picture__img');
   const pictureComment = template.querySelector('.picture__comments');
@@ -8,7 +10,7 @@ function createPictureThumbnail(template, {url, comments, likes}) {
   return template;
 }
 
-function viewImagesThumbnails(imagesData) {
+function viewImagesThumbnails(imagesData, clickImageHandler=noop) {
   const pictures = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture')
     .content
@@ -16,6 +18,10 @@ function viewImagesThumbnails(imagesData) {
   const fragment = document.createDocumentFragment();
   imagesData.forEach((photo) => {
     const newPictureMiniature = createPictureThumbnail(pictureTemplate.cloneNode(true), photo);
+    newPictureMiniature.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      clickImageHandler(photo);
+    });
     fragment.appendChild(newPictureMiniature);
   });
 
