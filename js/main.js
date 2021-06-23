e4b3e4f5840ee7a25f4f3b0a43dc1ab6d3bde998
sylvan008 <sylvan.climb@gmail.com>
2021-06-23 22:15:04 +0300

    increase comments per user photo

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ import {viewImagesThumbnails} from './view-images-thumbnails.js';
 import {viewFullImage} from './view-full-image.js';
 import {initUserModal} from './user-modal.js';
 
-const userPhotos = createStubPublishedPhoto(25, 3);
+const userPhotos = createStubPublishedPhoto(25, 15);
 viewImagesThumbnails(userPhotos, viewFullImage);
 initUserModal();
