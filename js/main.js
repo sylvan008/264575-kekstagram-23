11f547ad48d44dd0262e5f611d3320b727465361
sylvan008 <sylvan.climb@gmail.com>
2021-06-30 22:29:12 +0300

    get user's images and render thumbnails

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-import {createStubPublishedPhoto} from './data.js';
 import {viewImagesThumbnails} from './view-images-thumbnails.js';
 import {viewFullImage} from './view-full-image.js';
 import {initUserModal} from './user-modal.js';
+import {getData} from './api.js';
+import { viewErrorMessage } from './utils/error-message.js';
 
-const userPhotos = createStubPublishedPhoto(25, 15);
-viewImagesThumbnails(userPhotos, viewFullImage);
+const viewThumbnails = viewImagesThumbnails.bind(null, viewFullImage);
+
+getData(
+  () => viewErrorMessage('Не удалось загрузить фотографии. Попробуйте зайти позже.'),
+  viewThumbnails,
+);
+
 initUserModal();
+
+
